f77fbb9d971263c591f2769cbe698d5e6614f032
jadeDiucon <emailtoshivani@gmail.com>
2025-06-11 15:46:00 -0400

    Our Projects-Mimics Figma header+buttons section with limited button functionality. Has large spacing between sections. Note: no active button state or components used

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,17 +1,49 @@
 import React from 'react';
 import { Header } from '../components/Header';
 
+
 export default function Projects() {
     return (
         <>
-            <Header />
-            <div className="min-h-screen bg-[#8EB0D6] flex justify-center items-center">
-                <h1 className="text-6xl text-center text-[#2D336B] font-neuton mt-30 font-semibold tracking-widest">
-                    Projects
-                </h1>
-                    <div className="text-5xl text-center text-[#7886C7] font-neuton mt-20 font-semibold tracking-wider">
-                        "subheading"
-                    </div>
+            <Header /> 
+            <div className="min-h-screen bg-[#FFF2F2]">
+
+            <h1 className="text-6xl text-center text-[#2D336B] font-neuton mt-30 font-bold tracking-widest">
+                Our Projects
+            </h1>
+            <div className="text-5xl text-center text-[#7886C7] font-neuton mt-20 font-semibold tracking-wider">
+                    "subheading"
+            </div>
+
+            <div className="flex justify-center space-x-4"> 
+
+                <button className ="bg-[#2D336B] text-white border-3 border-[#2D336B] font-neuton mt-30 font-bold text-3xl 
+                    rounded-full hover:bg-[#2D336B] hover:text-white active:bg-[#2D336B] active:text-white w-34 h-13">
+                All
+                </button> 
+
+                <button className ="bg-[#FFF2F2] border-3 border-[#2D336B] text-[#2D336B] font-neuton mt-30 font-bold text-3xl rounded-full
+                    hover:bg-[#2D336B] hover:text-white active:bg-[#2D336B] active:text-white w-34 h-13">
+                2025
+                </button>  
+
+                <button className ="bg-[#FFF2F2] border-3 border-[#2D336B] text-[#2D336B] font-neuton mt-30 font-bold text-3xl rounded-full
+                    hover:bg-[#2D336B] hover:text-white active:bg-[#2D336B] active:text-white w-34 h-13">
+                Team X
+                </button>  
+
+                <button className ="bg-[#FFF2F2] border-3 border-[#2D336B] text-[#2D336B] font-neuton mt-30 font-bold text-3xl rounded-full
+                    hover:bg-[#2D336B] hover:text-white active:bg-[#2D336B] active:text-white w-34 h-13">
+                Team Y
+                </button>  
+
+                <button className ="bg-[#FFF2F2] border-3 border-[#2D336B] text-[#2D336B] font-neuton mt-30 font-bold text-3xl rounded-full
+                    hover:bg-[#2D336B] hover:text-white active:bg-[#2D336B] active:text-white w-34 h-13">
+                Team Z
+                </button>  
+
+            </div>
+
 
 
             </div>    
