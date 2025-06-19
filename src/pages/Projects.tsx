b408089aa2634b78137d5ff9a101802b9c5150c4
jadeDiucon <emailtoshivani@gmail.com>
2025-06-18 20:07:43 -0400

    Converted project page to  functions/components format

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,11 +2,10 @@ import React, { useState } from 'react';
 import { Header } from '../components/Header';
 import ProjButton from '../components/ProjButton';
 
-export default function Projects() {
-    return (
+function Middle() {
+    return(
         <>
-            <Header /> 
-            <div className="min-h-screen bg-[#FFF2F2]">
+        <div className="min-h-screen bg-[#FFF2F2]">
 
             <h1 className="text-6xl text-center text-[#2D336B] font-neuton mt-30 font-bold tracking-widest">
                 Our Projects
@@ -16,23 +15,28 @@ export default function Projects() {
             </div>
 
             <div className="flex justify-center space-x-4"> 
-                
-
                 <ProjButton label="All" />
                 <ProjButton label="2025" />
                 <ProjButton label="Team X" />
                 <ProjButton label="Team Y" />
                 <ProjButton label="Team Z" />
-{/* 
+            {/* 
                 <button className ="bg-[#2D336B] text-white border-3 border-[#2D336B] font-neuton mt-30 font-bold text-3xl 
                     rounded-full hover:bg-[#2D336B] hover:text-white active:bg-[#2D336B] active:text-white w-34 h-13">
                 All
                 </button> 
-*/}
-
+            */}
             </div>
-            </div> 
+        </div> 
+        </>
+    )
+}
 
+export default function Projects() {
+    return (
+        <>
+            <Header /> 
+            <Middle /> 
         </>
     );
 } 
