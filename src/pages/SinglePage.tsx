28b094472514dae3fd4d36d3bfe7aa2046f6fd3d
brthhule <brennenle@yahoo.com>
2025-06-19 02:34:58 -0400

    Added SinglePage file

---
+++ src/pages/SinglePage.tsx
@@ -0,0 +1,97 @@
+import React from 'React'
+
+
+
+
+
+function ClubNameSection() {
+    return (
+        <>
+            <div style={{backgroundColor: "beige"}}>
+                hello
+            </div>
+        </>
+    )
+}
+
+function WhoWeAreSection() {
+    return (
+        <>
+        </>
+    )
+}
+
+function GoalsSection() {
+    return (
+        <>
+        </>
+    )
+}
+
+function UppcomingMeetingsSection() {
+    return (
+        <>
+        </>
+    )
+}
+
+function WebsiteApplicationSection() {
+    return (
+        <>
+        </>
+    )
+}
+
+function WantToJoinSection() {
+    return (
+        <>
+        </>
+    )
+}
+function FAQSection() {
+    return (
+        <>
+        </>
+    )
+}
+
+function EventsSection() {
+    return (
+        <>
+        </>
+    )
+}
+
+function CrewSection() {
+    return (
+        <>
+        </>
+    )
+}
+
+function ContactUsSection() {
+    return (
+        <>
+        </>
+    )
+}
+
+
+
+export default function SinglePage() {
+    return (
+        <>
+            <ClubNameSection/>
+            <WhoWeAreSection/>
+            <GoalsSection/>
+            <UppcomingMeetingsSection/>
+            <WebsiteApplicationSection/>
+            <WebsiteApplicationSection/>
+            <WantToJoinSection/>
+            <FAQSection/>
+            <EventsSection/>
+            <CrewSection/>
+            <ContactUsSection/>
+        </>
+    )
+}
